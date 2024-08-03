a4ac42e0cafd28beea1add7914f99de5656b27e8
Keshav Khandelwal <117892493+keshavk21@users.noreply.github.com>
2024-08-03 20:17:24 +0000

    conversion data types

---
+++ datatypes-summery.js
@@ -0,0 +1,37 @@
+// # Primitive 
+
+
+//  7 types: String, Number, Boolean, Undefined, Null, Symbol, BigInt
+
+const score =100
+const scoreValue = 100.3
+
+const isLoggedIn = false
+const ourtsideTemp = null
+let userEmail;
+
+
+const id  = Symbol('123')
+const anotherId = Symbol('123')
+
+console.log(id === anotherId) // false\
+
+//const bigNumber = BigInt(12345678901234567890)
+
+// Java Script is a dynamic type and loosely typed language, so you don't have to specify the data type of the variable when you declare it.
+
+// # Reference(Non Primitive)
+
+// 10 types: Object, Array, Function, Date, RegExp, Map, Set, WeakMap, WeakSet, Error
+
+const heroes = ["shaktiman", "superman", "batman"]
+{
+    name: "keshav",
+    age =21
+};
+
+const myFunction=function(){
+    console.log("Hello world");
+}
+
+console.log(typeof heroes) 
