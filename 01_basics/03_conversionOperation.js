474b232a71c5325dafd5b4cd2c2da4fc3a38a601
Keshav Khandelwal <117892493+keshavk21@users.noreply.github.com>
2024-08-03 18:56:59 +0000

    commit: 01_basics/03_conversionOperation.js

---
+++ 01_basics/03_conversionOperation.js
@@ -0,0 +1,38 @@
+ let score = "Keshav"
+
+// console.log(typeof score);
+// console.log(typeof (score));
+
+ let valueInNumber = Number(score)
+// console.log(typeof valueInNumber);
+// console.log(valueInNumber);
+
+// "33"=>33
+// "33abc"=>NaN
+// true=>1
+// false=>0
+// null=>0
+// undefined=>NaN
+// ""=>0
+// " "=>0
+
+ let isLoggedIn = ""
+
+ let booleanIsLoggedIn = Boolean(isLoggedIn)
+// console.log(booleanIsLoggedIn);
+
+// 0=>false
+// 1=>true
+// -1=>true
+// ""=>false
+// " "=>true
+// null=>false
+// undefined=>false
+// NaN=>false
+//"Keshav"=>true
+
+let someNumber = 33
+
+let strinmgNumber = String(someNumber)
+console.log(strinmgNumber);
+console.log(typeof strinmgNumber);
